b45043d56d4ed33cb30cbbf12c230087703649c4
Yuanpeng Zhang <zyroc1990@gmail.com>
2021-07-01 14:06:33 -0400

    update topas4rmc

--- conda.recipes.3/bld.bat
+++ conda.recipes.3/bld.bat
@@ -1,6 +1,6 @@
 :: %PYTHON% -m easy_install --no-deps .
 :: if errorlevel 1 exit 1
-%PYTHON% setup.py install
+python setup.py install
 if errorlevel 1 exit 1
 
 set MENU_DIR=%PREFIX%\Menu
@@ -9,7 +9,7 @@ if not exist (%MENU_DIR%) mkdir %MENU_DIR%
 copy %RECIPE_DIR%\tr_icon.ico %MENU_DIR%
 if errorlevel 1 exit 1
 
-%PYTHON% %RECIPE_DIR%\expandpath.py %RECIPE_DIR%\menu-windows.json > %MENU_DIR%\menu-windows.json
+python %RECIPE_DIR%\expandpath.py %RECIPE_DIR%\menu-windows.json > %MENU_DIR%\menu-windows.json
 if errorlevel 1 exit 1
 
 :: copy %RECIPE_DIR%\menu-windows.json %MENU_DIR%\topas4rmc.json
